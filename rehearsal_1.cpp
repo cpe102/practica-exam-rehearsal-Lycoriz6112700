6ca7574bfa6c292fe2b6c320df975573d9d8bb71
lab314-28@labeng.local <lab314-28@labeng.local>
2020-01-10 15:02:21 +0700

    Alpha

--- rehearsal_1.cpp
+++ rehearsal_1.cpp
@@ -1,1 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float GPA;
+    string name;
+    cout << "What is your name?: ";
+    cin >> name;
+    cout << "What is your GPA?: ";
+    cin >> GPA;
+    if(GPA >= 3.50)
+    {
+        cout << name << " InW Jrim Jrim!!!";
+    }else{
+        cout << "Try harder, " << name << "!!!";
+    }
 
+    return 0;
+}
